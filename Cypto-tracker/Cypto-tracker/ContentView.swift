f535f79d457121fe7bdc779da4149e10408880f9
Annmarie-ds <annmarie.desilva@placie.com>
2024-01-16 10:34:07 +1100

    Created new swift ui project

---
+++ Cypto-tracker/Cypto-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cypto-tracker
+//
+//  Created by Annmarie De Silva on 16/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cypto-tracker/Cypto-tracker/Cypto_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cypto_trackerApp.swift
+//  Cypto-tracker
+//
+//  Created by Annmarie De Silva on 16/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cypto_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
